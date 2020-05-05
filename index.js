f95ddd7472ca22a4b74a852e1350f2739e7eecc3
stantbyZ <zz6210zz@6210.com>
2020-05-05 16:43:57 +0800

    v0.2

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-let state = {
-  count: 1
-}
-let listeners = []
-function subscribe(listener) {
-  listeners.push(listener)
-}
-
-function changeCount(count) {
-  state.count = count
-  for (let l of listeners) {
-    l()
+function createStore(initState) {
+  let state = initState
+  let listeners = []
+  // subscribe
+  function subscribe(listener) {
+    listeners.push(listener)
   }
+  // changeState
+  function changeState(newState) {
+    state = newState
+    for (let l of listeners) {
+      l()
+    }
+  }
+  // getState
+  function getState() {
+    return state
+  }
+  return { subscribe, getState, changeState }
 }
 
-/** use it */
+/** 管理多个状态 */
 
-subscribe(() => {
-  console.log(state.count)
+let initState = {
+  counter: { count: 0 },
+  info: { name: '', desc: '' }
+}
+let store = createStore(initState)
+// 订阅info
+store.subscribe(() => {
+  let state = store.getState()
+  console.log(`${state.info.name}: ${state.info.desc}`)
 })
-changeCount(2)
-changeCount(3)
-changeCount(4)
+// 订阅counter
+store.subscribe(() => {
+  let state = store.getState()
+  console.log(`counter: ${state.counter.count}`)
+})
+// change info
+store.changeState({
+  ...store.getState(),
+  info: {
+    name: 'jack',
+    desc: 'handsome guy'
+  }
+})
+// change count
+store.changeState({
+  ...store.getState(),
+  counter: { count: 43 }
+})
